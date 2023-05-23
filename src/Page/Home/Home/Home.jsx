19ca42fc8350f7acd0fb5da0ee0c4e9d2312bf96
soriful15 <sorifulislam.rs10@gmailcomm>
2023-05-23 21:30:25 +0600

    banner is complete

--- src/Page/Home/Home/Home.jsx
+++ src/Page/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Banner from '../Banner/Banner';
 const Home = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <section>
+                <Banner></Banner>
+
+            </section>
         </div>
     );
 };
